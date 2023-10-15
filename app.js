3c4f84b70f41e057866c20d31df78f56da7e17a4
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-10-15 13:09:18 +0530

    Added filter functionality to the contact list

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ function filterNames() {
     const filterText = filterInput.value.toUpperCase();
     const ul = document.getElementById('names');
     const li = ul.querySelectorAll('li.collection-item');
+
+    li.forEach((listItem) => {
+        const a = listItem.getElementsByTagName('a')[0];
+        if (a.innerHTML.toUpperCase().indexOf(filterText) < 0)
+            listItem.style.display = 'none';
+        else
+            listItem.style.display = '';
+    });
 }
